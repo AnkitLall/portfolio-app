aaf096875a565a73ae1d9439c2d5ca23028d7b09
Ankit <ankitlall@Ankits-MacBook-Pro.local>
2024-06-08 01:54:59 -0400

    clean up

--- src/styledComponents/styledToolBar.js
+++
@@ -1,8 +0,0 @@
-import styled from "@emotion/styled";
-import { Toolbar } from "@mui/material";
-
-export const StyledToolbar = styled(Toolbar)(({ theme }) => ({
-    '@media all': {
-      minHeight: 100,
-    },
-}));
