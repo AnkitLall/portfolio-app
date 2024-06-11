8b86d8db68bef152d51316c8ce4ead6be943429d
Ankit <ankitlall@Ankits-MacBook-Pro.local>
2024-06-10 20:13:23 -0400

    analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import './index.css';
+import { Analytics } from "@vercel/analytics/react"
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(<> <Analytics /><App /></>)
 
 // ReactDOM.createRoot(document.getElementById('root')).render(<App />);
